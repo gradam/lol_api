ebf68dc7491718f14ed29c7bf7fd2081760552c3
Jakub Semik <kuba.semik@gmail.com>
2016-05-25 11:48:38 +0000

    fixed

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 setup(
     name='lol_api',
-    version='0.1.2',
+    version='0.1.3',
     description='wrapper and utils for League of Legends API',
     long_description=readme(),
     url='https://github.com/gradam/lol_api',
